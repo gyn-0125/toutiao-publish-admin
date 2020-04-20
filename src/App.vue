51d456afde18fc48e09fc3126075b3d74212f9ca
用户名 <317991755@qq.com>
2020-04-20 12:21:03 +0800

    项目初始-导入Element

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
-    <h1>黑马头条</h1>
+    <h1>黑马头条内容发布系统</h1>
     <router-view />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {}
+  },
+  methods: {},
+  created () {}
 }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+</style>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
