ee6b41a35a7b871c6e11aa466d01c792b5b67eaa
用户名 <317991755@qq.com>
2020-04-25 11:14:17 +0800

    发布文章-完成页面

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="publish-container">发布文章</div>
+  <div class="publish-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item><a href="/">发布文章</a></el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="标题">
+          <el-input v-model="article.title"></el-input>
+        </el-form-item>
+        <el-form-item label="内容">
+          <el-input type="textarea" v-model="article.content"></el-input>
+        </el-form-item>
+        <el-form-item label="封面">
+          <el-radio-group v-model="form.resource">
+            <el-radio label="单图"></el-radio>
+            <el-radio label="三图"></el-radio>
+            <el-radio label="无图"></el-radio>
+            <el-radio label="自动"></el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="form.region" placeholder="请选择频道">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">发表</el-button>
+          <el-button>存入草稿</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,13 +44,36 @@ export default {
   props: {},
   components: {},
   data () {
-    return {}
+    return {
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      },
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: { // 文章封面
+          type: 0, // 文章类型
+          images: [] // 封面图片的地址
+        }
+      }
+    }
   },
   computed: {},
   watch: {},
   created () {},
   mounted () {},
-  methods: {}
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
 
