57b6191787fbfa335d17891f7dda54db0576bab6
用户名 <317991755@qq.com>
2020-04-29 17:38:39 +0800

    粉丝管理-粉丝列表完成页面组件

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -8,10 +8,25 @@
           <el-breadcrumb-item>粉丝管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
-      <div class="action-head">
-        <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
-        <div ref="main" style="width: 600px; height:400px;"></div>
-      </div>
+      <el-tabs v-model="activeName" type="card" @tab-click="handleClick">
+        <el-tab-pane label="粉丝列表" name="first">
+          <el-row :gutter="10">
+            <el-col class="fans-item">
+              <el-image class="fans-image"></el-image>
+              <p>名字</p>
+              <el-button>关注</el-button>
+            </el-col>
+          </el-row>
+          <el-pagination
+            layout="prev, pager, next"
+            :total="1000">
+          </el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="粉丝画像" name="second">
+          <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+          <div ref="main" style="width: 600px; height:400px;"></div>
+        </el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
@@ -24,7 +39,9 @@ export default {
   props: {},
   components: {},
   data () {
-    return {}
+    return {
+      activeName: 'first'
+    }
   },
   computed: {},
   watch: {},
@@ -55,8 +72,24 @@ export default {
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option)
   },
-  methods: {}
+  methods: {
+    handleClick () {
+      // console.log(tab, event)
+    }
+  }
 }
 </script>
 
-<style lang='less' scoped></style>
+<style lang='less' scoped>
+.fans-item {
+  border: 1px dashed #ddd;
+  width: 120px;
+  height: 170px;
+  text-align: center;
+  padding: 10px 0;
+  display: inline-block;
+  margin-left: 5px;
+  margin-right: 50px;
+  margin-bottom: 15px;
+}
+</style>
