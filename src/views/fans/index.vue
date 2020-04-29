a067f02913ee91da88acf241a894eb17d81b85dc
用户名 <317991755@qq.com>
2020-04-29 16:17:11 +0800

    粉丝管理-echarts的使用

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -1,8 +1,24 @@
 <template>
-  <div class="fans-container">粉丝管理</div>
+  <div class="fans-container">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <!-- 面包路径导航 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item to="/">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>粉丝管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="action-head">
+        <!-- 为 ECharts 准备一个具备大小（宽高）的 DOM -->
+        <div ref="main" style="width: 600px; height:400px;"></div>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+
 export default {
   name: 'FansIndex',
   props: {},
@@ -13,7 +29,32 @@ export default {
   computed: {},
   watch: {},
   created () {},
-  mounted () {},
+  mounted () {
+    // 初始化图表
+    var myChart = echarts.init(this.$refs.main)
+    // 指定图表的配置项和数据
+    var option = {
+      title: {
+        text: 'ECharts 入门示例'
+      },
+      tooltip: {},
+      legend: {
+        data: ['销量']
+      },
+      xAxis: {
+        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+      }]
+    }
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option)
+  },
   methods: {}
 }
 </script>
