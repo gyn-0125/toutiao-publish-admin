d21c61b7f982b60f31a27be33b6b667c0c0d046f
用户名 <317991755@qq.com>
2020-04-30 10:45:41 +0800

    文章封面处理-封面个数选择

---
+++ src/views/publish/components/upload-cover.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="upload-cover">文章封面</div>
+</template>
+
+<script>
+export default {
+  name: 'UploadCover',
+  props: {},
+  components: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang='less' scoped></style>
