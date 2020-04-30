df56870b60c6970c5af45e9f184f098cd0ce0d5a
用户名 <317991755@qq.com>
2020-04-30 17:00:20 +0800

    粉丝管理-粉丝画像图标处理完成

--- src/views/fans/index.vue
+++ src/views/fans/index.vue
@@ -73,24 +73,42 @@ export default {
     var myChart = echarts.init(this.$refs.main)
     // 指定图表的配置项和数据
     var option = {
-      title: {
-        text: 'ECharts 入门示例'
+      color: ['#3398DB'],
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: { // 坐标轴指示器，坐标轴触发有效
+          type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+        }
       },
-      tooltip: {},
-      legend: {
-        data: ['销量']
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
       },
-      xAxis: {
-        data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-      },
-      yAxis: {},
-      series: [{
-        name: '销量',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-      }]
+      xAxis: [
+        {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+          axisTick: {
+            alignWithLabel: true
+          }
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          name: '直接访问',
+          type: 'bar',
+          barWidth: '60%',
+          data: [10, 52, 200, 334, 390, 330, 220]
+        }
+      ]
     }
-
     // 使用刚指定的配置项和数据显示图表。
     myChart.setOption(option)
   },
